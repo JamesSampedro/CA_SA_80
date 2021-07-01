b15a780d53163676c19910cc581b232a2659d5c3
James.Sampedro <72242960+Votum38git@users.noreply.github.com>
2021-07-01 16:12:25 +0800

    Add project files.

---
+++ cash_advance_salary_loan/Connection.vb
@@ -0,0 +1,3 @@
+﻿Public Class Connection
+    Public connectionString As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\YourName\Desktop\cd\cash_advance_salary_loan_user_authorizer_admin-COMPLETE-master\cash_advance_salary_loan.accdb"
+End Class
